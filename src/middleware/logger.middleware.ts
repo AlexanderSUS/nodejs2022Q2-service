265db2130ad0fb9f6301fbce79e646cbad55f664
alexandersus <sasha_suslov07@mail.ru>
2023-05-31 18:01:53 +0300

    refactor: logger middleware

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,10 +1,8 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import LogsService from '../logger/logs.service';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  constructor(private readonly loggerService: LogsService) {}
   private logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
@@ -13,16 +11,9 @@ export class LoggerMiddleware implements NestMiddleware {
     response.on('finish', () => {
       const log = `Time: ${new Date().toISOString()}, ${method} ${originalUrl} query=${JSON.stringify(
         query,
-      )} body=${JSON.stringify(body)}. Response: status code ${
-        response.statusCode
-      }`;
+      )} body=${JSON.stringify(body)}, status code ${response.statusCode}`;
 
       this.logger.log(log);
-      this.loggerService.saveLog({
-        context: 'LOG',
-        level: 'HTTP',
-        message: log,
-      });
     });
 
     next();
